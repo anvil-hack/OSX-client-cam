e4d0039e3b29f02569d4c4ffe3b354855c707bed
remi <remirobert33530@gmail.com>
2017-04-22 14:15:19 +0100

    add base project

---
+++ OSX-client-cam/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OSX-client-cam
+//
+//  Created by Remi Robert on 22/04/2017.
+//  Copyright © 2017 Remi Robert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OSX-client-cam/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OSX-client-cam
+//
+//  Created by Remi Robert on 22/04/2017.
+//  Copyright © 2017 Remi Robert. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
